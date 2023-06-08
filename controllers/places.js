c70ac2ebcaf498d244ab6432f49d3905426c1681
jaylinrose <jaylin.manuel02@gmail.com>
2023-06-07 21:42:22 -0700

    fixed edit submit load bug

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,58 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get('/', (req, res) => {
-    res.render('places/index', {places})
-})
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
+router.get('/:id/edit',(req,res) =>{
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')
+  }
+  else if (!places[id]){
+    res.render('error404')
+  }
+  else{
+
+      res.render('places/edit',{place: places[id],id:id})
+  }
+
+  
+
+})
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+ 
+ 
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = '/images/50s.jpg'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+ 
+      // Save the new data into places[id]
+     places[id] = req.body
+  res.redirect(`/places/${id}`)
+  }
+})
+
+
+
 
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -18,31 +63,36 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id], id })
+    res.render('places/show', {places: places[id], id})
   }
 })
 
-router.get('/:id/edit', (req, res) => {
+router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-      res.render('error404')
+    res.render('error404')
   }
   else if (!places[id]) {
-      res.render('error404')
+    res.render('error404')
   }
   else {
-    res.render('places/edit', { place: places[id] })
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
 
 
-router.post('/', (req,res) => {
+
+
+
+router.post('/', (req, res) => {
+  console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = 'images/50s.jpg' 
   }
   if (!req.body.city) {
-  req.body.city = 'Anytown'
+    req.body.city = 'Any-town'
   }
   if (!req.body.state) {
     req.body.state = 'USA'
@@ -51,19 +101,10 @@ router.post('/', (req,res) => {
   res.redirect('/places')
 })
 
-  router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-     if (isNaN(id)) {
-      res.render('error404')
-     }
-     else if (!places[id]) {
-       res.render('error404')
-     }
-    else {
-       places.splice(id, 1)
-       res.redirect('/places')
-     }
-     })
+router.get('/', (req, res) => {
+    res.render('places/index', {places})
+})
+
 
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,23 +2,34 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return(
+    return (
         <Def>
-            <main>
-                <h1>{data.place.name}</h1>
-                <a href={`/places/${data.id}/edit`} className= "btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
-            </main>
+          <main>
+            <h1>{data.places.name}</h1>
+          </main>
+          <body><center>
+            <img src={data.places.pic} alt="Place Image" />
+            <br></br>
+            <h2>description</h2> 
+            {data.places.city}, {data.places.state}
+            <br></br>
+            {data.places.cuisines}        
+            <h2>Rating</h2>
+            currently unrated.
+            <h2>Comments</h2>
+            No comments yet!
+            <br></br>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+              </a>  
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+              </form>     
+            </center></body>
         </Def>
-
     )
-
 }
 
 module.exports = show
