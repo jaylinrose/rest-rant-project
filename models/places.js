bee14e947889a549339eef2b53e7b5b3175e7354
jaylinrose <jaylin.manuel02@gmail.com>
2023-06-12 18:22:33 -0700

    fixed show page error and corrected URI

--- models/places.js
+++ models/places.js
@@ -1,15 +1,12 @@
+const mongoose = require('mongoose')
 
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cat-cafe.jpg'
-  }]
-  
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
