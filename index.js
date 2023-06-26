cd1425d2e3908fa5877103f913f680b864b79367
jaylinrose <jaylin.manuel02@gmail.com>
2023-06-25 19:11:54 -0700

    added and updated index.js w/ controllers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -20,10 +21,20 @@ app.use(express.urlencoded({extended: true}))
 // Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
-app.get('*', (req, res) => {
-res.render('error404')
-})
-    
+const breadsController = require('./controllers/breads_controller') 
 
-//Listen for Connections
+app.use('/breads', breadsController) 
+
+//BAKERS 
+
+const bakersController = require('./controllers/bakers_controller') 
+app.use('/bakers', bakersController) 
+const PORT = process.env.PORT || 5000 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {console.log('connected to : ', process.env.MONGO_URI)} ) 
+
+// 404 Page 
+
+app.get('*', (req, res) => { res.send('404') }) 
+
 app.listen(process.env.PORT)
+
